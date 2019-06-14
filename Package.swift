90014ceb4de40411b333ea684f0c6347458a9b49
Ben Lachman <ben@nicemohawk.com>
2019-06-14 16:43:24 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
-  name: "Locksmith"
+    name: "Locksmith",
+    products: [
+        .library(
+            name: "Locksmith",
+            targets: ["Locksmith"])
+    ],
+    targets: [
+        .target(
+            name: "Locksmith",
+            path: "Source")
+    ],
+    swiftLanguageVersions: [.v3, .v4]
 )
